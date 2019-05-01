09b0643df99a23f6aee6e1ad803e4d663807a5ed
Don Snyder <DonRSnyder@gmail.com>
2019-05-01 13:49:39 -0400

    Added basicPTR.cpp

---
+++ Pointers/basicPTR.cpp
@@ -0,0 +1,31 @@
+/*
+	File:  basicPTR.cpp
+
+	Author: Don Snyder
+
+	Function: C++ program to demonstrate basic memory pointer operation.
+
+		  The primary demonstration is the calling a std::thread operation...providing
+		  three different examples on how to call the std::thread.
+
+		  NOTE THE LAMBDA EXPRESSION USE AS THE THIRD EXAMPLE FOR STARTING A std::thread
+
+	Credit:  The foundation of this example is found here -
+			http://www.cplusplus.com/doc/tutorial/pointers/
+*/
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+  int firstvalue, secondvalue;
+  int * mypointer;
+
+  mypointer = &firstvalue;
+  *mypointer = 10;
+  mypointer = &secondvalue;
+  *mypointer = 20;
+  cout << "firstvalue is " << firstvalue << '\n';
+  cout << "secondvalue is " << secondvalue << '\n';
+  return 0;
+}
